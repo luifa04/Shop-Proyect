1d5f7a864a1d4aa0f67fb391230f5ecd6d1d648d
mateo <mateo@MSI.localdomain>
2024-01-09 18:15:55 -0300

    config2

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
-  dbUrl: process.env.DATABASE_URL || 'postgres://admin:admin123@localhost:5432/my_store',
+  dbUrl: 'postgres://admin:admin123@localhost:5432/my_store',
   apikey: process.env.APIKEY,
   jwtSecret: process.env.JWT_SECRET || K4M5N7Q8R9SBUCVDXFYGZJ3K4M6P7Q8SATBUDWEXFZH2J3M5N6P8R9SATC,
   email: process.env.EMAIL,
